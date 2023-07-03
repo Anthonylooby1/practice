b585fa0d7c3f17bf98acbab6c0619c61bf10efb9
Anthonylooby1 <anthonylooby@yahoo.com>
2023-07-03 18:33:23 -0400

    functions2

---
+++ functions2.py
@@ -0,0 +1,48 @@
+# def number(x):
+#     new_list = []
+#     for x in range(5,-1,-1):
+#         new_list.append(x)
+        
+#     print(new_list)    
+        
+# number(5)   
+
+
+
+# def num_p(x,y):
+#     print(x)
+#     return y
+
+# we_print = num_p(5,3)
+# print(we_print)
+
+
+
+# def plus_value(x):
+#     value = len(x) + x[0]
+#     return value
+# new_list = plus_value([1,2,3,4,5])    
+# print(new_list)
+
+
+
+
+# def greater_than(x):
+#     new_list = []
+#     for i in range(0,len(x),1):
+#         if x[i] > x[i] + 1:
+#             new_list.append([i])
+#             print(x[i])
+# greater_than([5,2,3,2,1,4])   
+
+
+
+# def new_list_value(x,y):
+#     ex_list = []
+#     for i in range(0,4,1):
+#         ex_list.append(y)
+#         # print(ex_list)
+        
+#     return ex_list
+# multi_list = new_list_value(4,7)    
+# print(multi_list)
