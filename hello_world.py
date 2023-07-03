e37c3dd76e4365af88d770f8fe776b907c1ac6fa
Anthonylooby1 <anthonylooby@yahoo.com>
2023-07-03 16:11:25 -0400

    python practice

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+
+my_name = "Anthony"
+print(f"Hello {my_name}")
+
+print("hello" + my_name)
+
+fav_num = 7
+print("hello", fav_num)
+
+print(f"Hello {fav_num}")
+
+fav_food1 = "katsudon"
+fav_food2 = "gyudon"
+print("My favorite foods are {} and {}".format(fav_food1,fav_food2))
+print(f"I love to eat {fav_food1} and {fav_food2}")
