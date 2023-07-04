798c96c410ad0780e4e57184cbe828a61ab0cd30
Anthonylooby1 <anthonylooby@yahoo.com>
2023-07-04 15:23:31 -0400

    BankAccount

---
+++ BankAccount.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    
+    def __init__(self, int_rate, balance): 
+        self.balance = balance
+        self.int_rate = int_rate
+
+account = BankAccount(4,0)    
+
+def deposit(self, amount):
+        account.balance = account.balance + amount
+def withdraw(self, amount):
+        account.balance = account.balance - amount
+        if account.balance < 0:
+            account.balance -= 5
+            print("insufficient funds: Chargin a $5 fee")
+def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+def yield_interest(self):
+        self.balance * self.int_rate
+
+
+deposit(10,10)        
+        
+
